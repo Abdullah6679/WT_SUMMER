18073592faede5dc7f56de6153cc246cadd59aac
Abdullah6679 <106831218+Abdullah6679@users.noreply.github.com>
2022-06-04 22:04:41 +0600

    Add files via upload

---
+++ lab1.php
@@ -0,0 +1,113 @@
+<html>
+    <head>
+        <title>
+            Registration Form
+        </title>
+        <body>
+            <h1>Registration Form</h1>
+           
+
+
+       <form >
+           <table>
+               <tr>
+                    <td>
+                        First Name
+                    </td>       
+                    <td>
+                        <input type="text">
+                    </td>
+               </tr>    
+               <tr>
+                    <td>
+
+                        Last Name 
+                    </td>
+                    <td>
+
+                         <input type="text">
+                    </td>    
+                </tr>
+
+                <tr>
+                    <td>
+
+           Designation
+                  </td>
+           
+                  <td> 
+                       <input type="radio" name= "Designation">Junior Programmer
+                        
+                  
+                 
+           
+                  <input type="radio" name= "Designation">Senior Programmer
+           </td>
+           </tr>
+
+           <tr>
+                    <td>
+
+           Preffered language
+                  </td>
+           
+                  <td> 
+                       <input type="checkbox" name= "Preffered language">JAVA
+                        
+                 
+                  
+           
+           
+
+     
+           
+          
+                      
+                       <input type="checkbox" name= "Preffered language">C++
+                       <input type="checkbox" name= "Preffered language">PHP
+    </td>
+           </tr>
+
+           <tr>
+                    <td>
+
+                        E mail
+                    </td>
+                    <td>
+
+                         <input type="text">
+                    </td>    
+                </tr>
+
+                <tr>
+                    <td>
+
+                        Password
+                    </td>
+                    <td>
+
+                         <input type="password">
+                    </td>    
+                </tr>
+
+                <tr>
+                    <td>
+
+                    <p>Please choose a file <input type ="button" value ="Choose File"> No file choosen</p></td></tr>
+                   
+                    </td>
+                     
+                </tr>
+          
+
+<tr>
+    <td>
+       <input type="submit" value ="Submit">
+       <input type="submit" value ="Reset">
+</td>
+</tr>
+       
+</table>
+       </form>
+</body>
+</html>
