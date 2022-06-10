f563af00b75f70eec13824dfadf0bb305fe2d779
Abdullah6679 <106831218+Abdullah6679@users.noreply.github.com>
2022-06-10 12:13:55 +0600

    Lab Task 2

---
+++ Labtask2/control/process.php
@@ -0,0 +1,78 @@
+<?php
+$name=$_REQUEST["firstnamename"];
+if(empty($name)){
+    echo"<br>Your first name should not be empty";
+}
+else if(strlen($name)<4)
+{
+    echo "<br>First name should be more than 4 characters";
+}
+else{
+    echo"<br>Your first name is ".$name;
+}
+$name2=$_REQUEST["lname"];
+if(strlen($name2)<4)
+if(empty($name2)){
+    echo"<br>Your last name should not be empty";
+}
+else if(strlen($name2)<4)
+{
+    echo "<br>Last name should be more than 4 characters";
+}
+else{
+    echo"<br>Your Last name is ".$name2;
+}
+
+if(isset($_REQUEST["des"]))
+{
+    $des=$_REQUEST["des"];
+    echo "<br>Your designation is ".$des;
+}
+else{
+    echo "<br>You must select designation";
+}
+
+if(isset($_REQUEST["Jrpro"]) || isset($_REQUEST["Srpro"]) || isset($_REQUEST["Prlead"]))
+{
+    if(isset($_REQUEST["Jrpro"]) && isset($_REQUEST["Srpro"]) && isset($_REQUEST["Prlead"]) ){
+        echo "<br> You have selected ".$_REQUEST["Jrpro"]." and ".$_REQUEST["Srpro"]." and ".$_REQUEST["Prlead"];
+    }
+    else if(isset($_REQUEST["Jrpro"]) && isset($_REQUEST["Srpro"])){
+        echo "<br> You have selected ".$_REQUEST["Jrpro"]." and ".$_REQUEST["Srpro"];
+    }
+    else if(isset($_REQUEST["Srpro"]) && isset($_REQUEST["Prlead"])){
+        echo "<br> You have selected ".$_REQUEST["Srpro"]." and ".$_REQUEST["Prlead"];
+    }
+    else if(isset($_REQUEST["Jrpro"]) && isset($_REQUEST["Prlead"])){
+        echo "<br> You have selected ".$_REQUEST["Jrpro"]." and ".$_REQUEST["Prlead"];
+    }
+    else if(isset($_REQUEST["Jrpro"])){
+        echo "<br> You have selected ".$_REQUEST["Jrpro"];
+    }
+    else if(isset($_REQUEST["Srpro"])){
+        echo "<br> You have selected ".$_REQUEST["Srpro"];
+    }
+    else{
+        echo "<br> You have selected ".$_REQUEST["Prlead"];
+    }
+}
+else{
+    echo "<br>You must select preferance";
+}
+
+if(empty($_REQUEST["male"])){
+    echo "<br>Email should not be empty";
+}
+else{
+    echo "<br>Your email is :".$_REQUEST["male"];
+}
+
+if(is_numeric($_REQUEST["password"])<2){
+    echo "<br>Your password is valid";
+}
+else{
+    echo "<br>Your password must contain 1 numerical value";
+}
+
+
+?>
---
+++ Labtask2/view/file1.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <title>Registration Form</title>
+</head>
+<body>
+    <h1>Registration Form</h1>
+     <form action="../control/process.php" method="POST">
+         <table>
+             
+         <tr>
+             <td>First Name:</td>
+
+       <td> <input type="text"  name="firstname"></td></tr>
+
+       <tr>
+           <td>Last Name:</td> 
+       <td> <input type="text"  name="lastname"></td></tr>
+       <tr>
+           <td>Age:</td> 
+       <td> <input type="text"  name="age"></td>
+    </tr>
+       
+
+       <tr>
+           <td>Designation:</td>
+       <td><input type="radio" name="des" value="Jrpro">Junior Programmer 
+       <input type="radio" name="des" value="Srpro">Senior Programmer 
+       <input type="radio" name="des" value="Prlead">Project Lead</td>
+    </tr>
+        
+
+        <tr>
+            <td>Preferred Language:</td>
+       <td><input type="checkbox" name="pref1" value="pref1">Java 
+       <input type="checkbox" name="pref2" value="pref2">PHP 
+       <input type="checkbox" name="pref3" value="pref3">C++</td>
+    </tr>
+
+       <tr>
+           <td>Email:</td> 
+       <td> <input type="text"  name="male"></td></tr>
+       <tr><td>Password:</td> 
+       <td> <input type="password"  name="password"></td></tr>
+       <tr><td>Please choose file </td>
+        <td><input type="file"  name="id"></td>
+    </tr>
+       
+       <tr>
+            <td><input type="submit" value="Submit">
+        <input type="submit" value="Reset"></td> 
+    </tr>
+
+</table>
+
+</form>
+
+</body>
+</html>
